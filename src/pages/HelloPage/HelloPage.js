5477c28c24b4af3360053ed443a96e1fe3855c82
dimajun <drankendmitrij@gmail.com>
2024-08-09 11:16:15 +0300

    pages: Changed HelloPage content

--- src/pages/HelloPage/HelloPage.js
+++ src/pages/HelloPage/HelloPage.js
@@ -1,9 +1,23 @@
-import styles from './HelloPage.module.scss';
+import styles from "./HelloPage.module.scss";
+import { ReactTyped } from "react-typed";
 
 export const HelloPage = () => {
-    return (
-        <div className={styles.HelloPage}>
-            Hello!
-        </div>
-    )
+  return (
+    <div className={styles.HelloPage}>
+      <ReactTyped
+        backSpeed={50}
+        strings={["<h1>Welcome to the Budget App</h1>"]}
+        showCursor={false}
+        typeSpeed={50}
+      />
+      <ReactTyped
+        backSpeed={50}
+        strings={[
+          "<p>My name is Dima and this is my pet project for my portfolio! <a href='https://github.com/DimaJun'>GitHub</a></p>",
+        ]}
+        showCursor={false}
+        typeSpeed={50}
+      />
+    </div>
+  );
 };
